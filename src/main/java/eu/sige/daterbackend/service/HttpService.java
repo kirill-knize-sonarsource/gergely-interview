963fc217305202cb99d69e8c73d36aadd8367766
Simon Gergely <gergely.zsolt.simon@ibm.com>
2022-08-08 11:48:24 +0200

    ensure that http service is singleton

--- src/main/java/eu/sige/daterbackend/service/HttpService.java
+++ src/main/java/eu/sige/daterbackend/service/HttpService.java
@@ -1,9 +1,12 @@
 package eu.sige.daterbackend.service;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class HttpService {
 
     private final OkHttpClient okHttpClient;
